837ac90eff198e4ebf3d4ec7c4976a9e22ed8c69
Ивайло Пръмов <ivaylopr@gmail.com>
2025-06-25 15:15:24 +0300

    Main menu component added

---
+++ TourOperatorF5System/Components/MainMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourOperatorF5System.Components
+{
+    public class MainMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
