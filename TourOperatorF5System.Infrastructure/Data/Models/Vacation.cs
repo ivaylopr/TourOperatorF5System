283144a773f397a91289d2dff9e72d8b55d87ca8
Ивайло Пръмов <ivaylopr@gmail.com>
2025-06-26 08:54:22 +0300

    Models added and vacation model created

---
+++ TourOperatorF5System.Infrastructure/Data/Models/Vacation.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static TourOperatorF5System.Infrastructure.Constants.DataConstants;
+
+namespace TourOperatorF5System.Infrastructure.Data.Models
+{
+    public class Vacation
+    {
+        [Comment("Vacation identifier")]
+        [Key]
+        public int Id { get; set; }
+
+        [Comment("Vacation title")]
+        [Required]
+        [MaxLength(VacationTitleMaxLenght)]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(VacationInfoMaxLenght)]
+        public string VacationInfo { get; set; } = string.Empty;
+
+        [Comment("Start date of the vacation")]
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Comment("End date of the vacation")]
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        [Comment("Capacity for the vacation")]
+        [Required]
+        public int VacationCapacity { get; set; }
+
+        [Comment("Hotel of the vacation")]
+        [Required]
+        public Hotel Hotel { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Hotel))]
+        public int HotelId { get; set; }
+        [Comment("Agent responsible for the vacation")]
+        [Required]
+        public Agent Agent { get; set; } = null!;
+
+        [Required]
+        [ForeignKey(nameof(Agent))]
+        public int AgentId { get; set; }
+        [Required]
+        public decimal TotalPrice { get; set; }
+
+        public ICollection<Customer> Custromenrs { get; set; } = new List<Customer>();
+
+    }
+}
