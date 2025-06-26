a86947b8c60a9111410c904d90bebf81bfeb6790
Ивайло Пръмов <ivaylopr@gmail.com>
2025-06-26 08:54:43 +0300

    data constants separated and added

---
+++ TourOperatorF5System.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourOperatorF5System.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int VacationTitleMaxLenght = 50;
+        public const int VacationInfoMinLenght = 20;
+        public const int VacationInfoMaxLenght = 200;
+    }
+}
